39e9135cabaf5df7c32fe9377e841e2912e81388
Cassidy <cassidy.rouelle@gmail.com>
2024-02-13 09:53:22 +0100

    Cass → form html

---
+++ main.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <form class="form">
+        <label for="name">Name : </label><input type="text" name="name" class="name">
+        <label for="firstname">Firstname : </label><input type="text" name="firstname" class="firstname">
+        <label for="email">Email adress : </label><input type="email" name="email" class="email">
+        <label for="file">Uplade your picture : </label><input type="file" name="file" class=="file">
+        <label for="desc">Give us a description of your probleme : </label><input type="text" name="desc" class="desc">
+        <button type="submit">Send</button>
+    </form>
+    
+</body>
+</html>
