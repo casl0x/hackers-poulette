1144cfa99f3cbedf1a026ea073d0cf7a54f59e43
Cassidy <cassidy.rouelle@gmail.com>
2024-02-14 10:38:23 +0100

    Cass → design start

--- dashboard.php
+++ dashboard.php
@@ -5,9 +5,10 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Admin Dashboard</title>
 </head>
-<body>
-    <h1>Admin Dashboard</h1>
+<body class="flex items-center justify-center w-screen flex-col">
+    <h1 class="text-4xl font-semi-bold tracking-tight text-black sm:text-4xl">Admin Dashboard</h1>
 
+    <div class="w-1/3 my-14">
     <?php
     $servername = "localhost";
     $username = "root";
@@ -24,12 +25,14 @@
 
       $requete = $conn->query('SELECT * FROM formulaire');
 
-      echo "<table border='1'>";
-      echo "<tr><th>Name</th><th>Firstname</th><th>Email</th><th>Picture</th><th>Description</th></tr>";
+      echo "<table class='border-2 border-black'>";
+      echo "<tr><th class=''>Name</th><th class=''>Firstname</th><th class=''>Email</th><th class=''>Picture</th><th class=''>Description</th><th class=''>Status</th></tr>";
       while ($donnees = $requete->fetch()) {
-          echo "<tr><td>".$donnees['nom']."</td><td>".$donnees['prenom']."</td><td>".$donnees['email']." Km</td><td>".$donnees['deposer']."</td><td>".$donnees['height_difference']."</td><td>".$donnees['description']."</td></tr>";
+          echo "<tr><td>".$donnees['nom']."</td><td>".$donnees['prenom']."</td><td>".$donnees['email']." Km</td><td>".$donnees['deposer']."</td><td>".$donnees['description']."</td></tr>";
       }
       echo "</table>";
     ?>
+    </div>
+    <script src="https://cdn.tailwindcss.com"></script>
   </body>
 </html>
